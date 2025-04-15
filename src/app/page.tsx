7bdff75f7a06cb3bc0e87560830a6677217a76da
Sara Meixner <smeixner@tollbrothers.com>
2025-04-14 22:26:08 -0400

    starting to get page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>Sara Meixner | Web Developer</main>
+      <main className={styles.main}>
+        <section className={`${styles.section} ${styles.hero}`}>
+          <div className={styles.heroImage}>
+            <img src={'./images/cover-photo.jpg'} alt={'Sara Meixner Cover Photo'} />
+            <div className={styles.overlay} />
+          </div>
+          <h1>Sara Meixner | Web Developer</h1>
+        </section>
+      </main>
       <footer className={styles.footer}>
         <Link href='https://github.com/sarameix'>GitHub</Link>
       </footer>
